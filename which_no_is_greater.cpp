85e059dcb69d466f87d428934eb8345a09987ddc
Abid Ali <111629829+yesabidali@users.noreply.github.com>
2025-07-29 18:51:13 +0530

    Add files via upload

---
+++ which_no_is_greater.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int minoftw(int a, int b){
+
+     if (a>b){
+    	cout<<"a is larger no";
+	 }
+	 else{
+		cout<<"b is greater";
+	 }
+}
+  
+int main(){
+	minoftw(9,19);
+}
